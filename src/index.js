51fd4167e8d1c9285ae5ef810a40d592984db51d
Ethan Jones <ejones4224@gmail.com>
2023-05-07 11:43:59 -0700

    cpmplete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+  });
+  
+});
+const taskList = document.querySelector('#tasks');
+
+function addDeleteButton(task) {
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'delete';
+  deleteButton.addEventListener('click', function() {
+    taskList.removeChild(task);
+  });
+  task.appendChild(deleteButton);
+}
+
+
+function addTask(description) {
+  const task = document.createElement('li');
+  task.innerText = description;
+  addDeleteButton(task); 
+  taskList.appendChild(task);
+}
+
+const form = document.querySelector('#create-task-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const description = document.querySelector('#new-task-description').value;
+  addTask(description);
 });
